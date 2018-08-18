5130d74d1cac6932297cfc69ee7ff9097eeafce2
slicing <sunjiaoyong@xiyouant.org>
2018-08-18 15:36:34 +0800

    GcoceThread commit

--- src/com/Slicing/ThreadGrocei/GroceriesThread.java
+++ src/com/Slicing/ThreadGrocei/GroceriesThread.java
@@ -1,36 +1,39 @@
 package ThreadGrocei;
 
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.Scanner;
 
 public class GroceriesThread implements Runnable {
     private static int sum = 0;
+    private static int count = 0;
     @Override
     public void run() {
         Commoditys commoditys = new Commoditys();
         int index = (int)(1+Math.random()*9);
         Commodity commodity = commoditys.commodities.get(index);
         sum += commodity.getPrice();
-        System.out.println(Thread.currentThread().getName() + "   购买了  " +  commodity.getName() + "  " + commodity.getPrice() + "   " + sum);
-        //System.out.println(sum);
+        count ++;
+        System.out.println(commodity.getName() + "  " + commodity.getPrice());
     }
 
-    public static void main(String[] args) {
-        ExecutorService executorService = Executors.newFixedThreadPool(2);
-        for (int i = 0;i<5;i++){
-            Thread thread = new Thread(new GroceriesThread(),"小明");
-            executorService.execute(thread);
+    public static void main(String[] args) throws InterruptedException {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入您的购物昵称：");
+        String name = scanner.nextLine();
+        System.out.println("请输入您即将购物个数：");
+        int num = scanner.nextInt();
+        GroceriesThread.thread(name,num);
+        System.out.println("今天共出售了" + count+ "件物品， " + "共" + sum + "元");
+
+
+    }
+    public static void thread(String name,int num) throws InterruptedException {
+        System.out.println("您购买了：");
+        for (int i = 0;i < num;i++){
+            Thread thread = new Thread(new GroceriesThread(),name);
+            thread.start();
+            thread.join();
         }
-        //System.out.println(sum);
-        executorService.shutdown();
-
-        /*Thread thread = new Thread(new GroceriesThread(),"小明 ");
-        Thread thread1 = new Thread(new GroceriesThread(),"小红 ");
-        thread.start();
-        thread1.start();*/
-        //System.out.println(Thread.currentThread().getName());
-
-
+        System.out.println(name + " 购买了"+count+"件物品,"+"共消费：" + sum + "元") ;
     }
 }
